199b6021128f0311e91a6a888ea56a9f21450ed2
KatyaBolta4eva <cat.bolta4eva@yandex.ru>
2026-04-29 19:32:40 +0300

    zooshop_change

--- zooshop-backend/app.js
+++ zooshop-backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.static("../frontend/build"));
+app.use(express.static("../zooshop-frontend/build"));
 
 app.use("/", routes);
 
